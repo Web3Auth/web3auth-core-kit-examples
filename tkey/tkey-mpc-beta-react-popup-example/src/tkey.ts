fe25d1f6c19080bed532ec6a0f89092f174a32a6
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-03-17 13:43:26 -0700

    tkey-mpc: remove extra tkey packages

--- tkey/tkey-mpc-beta-react-popup-example/src/tkey.ts
+++ tkey/tkey-mpc-beta-react-popup-example/src/tkey.ts
@@ -1,8 +1,6 @@
-import ThresholdKey from "@tkey/default";
-import SecurityQuestionsModule from "@tkey/security-questions";
+import ThresholdKey from "@tkey/core";
 import { TorusServiceProvider } from "@tkey/service-provider-torus";
 import { TorusStorageLayer } from "@tkey/storage-layer-torus";
-import WebStorageModule from "@tkey/web-storage";
 
 // Configuration of Service Provider
 
@@ -19,17 +17,10 @@ const storageLayer = new TorusStorageLayer({
   enableLogging: true,
 });
 
-// Configuration of Modules
-const webStorageModule = new WebStorageModule();
-const securityQuestionsModule = new SecurityQuestionsModule();
 
 // Instantiation of tKey
 export const tKey = new ThresholdKey({
   enableLogging: true,
-  modules: {
-    webStorage: webStorageModule,
-    securityQuestions: securityQuestionsModule,
-  },
   serviceProvider: torusSp as any,
   storageLayer: storageLayer as any,
   manualSync: true,
