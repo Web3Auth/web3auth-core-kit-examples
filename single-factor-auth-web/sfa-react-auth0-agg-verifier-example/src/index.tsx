6dfe08f4c6895199df07400cab1d85402bb5674e
Maharshi Mishra <ihsraham27@gmail.com>
2023-08-14 06:14:55 +0530

    WIP first commit

---
+++ single-factor-auth-web/sfa-react-auth0-agg-verifier-example/src/index.tsx
@@ -0,0 +1,26 @@
+
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <Auth0Provider
+    domain="shahbaz-torus.us.auth0.com"
+    clientId="yeQlVRUCS0OUOvL7W1ZSfwwTpg4HDdC3"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+      <App />
+  </Auth0Provider>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
