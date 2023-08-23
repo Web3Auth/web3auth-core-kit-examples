ab2dd6996898d9499ee8b4276495c858f6e0f845
Maharshi Mishra <ihsraham27@gmail.com>
2023-08-24 01:21:58 +0530

    next JWT example added

---
+++ single-factor-auth-web/sfa-next-jwt-example/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/maharshi/Documents/sfa-next-jwt-example/sfa-next-jwt-example/app/layout\":[\"static/media/2aaf0723e720e8b9-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
---
+++ single-factor-auth-web/sfa-next-jwt-example/app/page.tsx
@@ -0,0 +1,260 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// Import Single Factor Auth SDK for no redirect flow
+import { Web3Auth } from "@web3auth/single-factor-auth";
+import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+// RPC libraries for blockchain calls
+// import RPC from "./evm.web3";
+import RPC from "./evm.ethers";
+
+import Loading from "./Loading";
+import "./App.css";
+
+const verifier = "w3a-jwt-for-sfa-web";
+
+const clientId =
+  "BEglQSgt4cUWcj6SKRdu5QkOXTsePmMcusG5EAoyjyOYKlVRjIF1iCNnMOTfpzCiunHRrMui8TIwQPXdkQ8Yxuk"; // get from https://dashboard.web3auth.io
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0x5",
+  rpcTarget: "https://rpc.ankr.com/eth_goerli",
+  displayName: "Goerli Testnet",
+  blockExplorer: "https://goerli.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+};
+
+function App() {
+  const [web3authSFAuth, setWeb3authSFAuth] = useState<Web3Auth | null>(null);
+  const [usesSfaSDK, setUsesSfaSDK] = useState(false);
+  const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
+    null
+  );
+  const [idToken, setIdToken] = useState<string | null>(null);
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        // Initialising Web3Auth Single Factor Auth SDK
+        const web3authSfa = new Web3Auth({
+          clientId, // Get your Client ID from Web3Auth Dashboard
+          web3AuthNetwork: "testnet", // ["cyan", "testnet"]
+          usePnPKey: false, // Setting this to true returns the same key as PnP Web SDK, By default, this SDK returns CoreKitKey.
+        });
+        setWeb3authSFAuth(web3authSfa);
+        const provider = new EthereumPrivateKeyProvider({
+          config: { chainConfig },
+        });
+
+        web3authSfa.init(provider);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const parseToken = (token: any) => {
+    try {
+      const base64Url = token.split(".")[1];
+      const base64 = base64Url.replace("-", "+").replace("_", "/");
+      return JSON.parse(window.atob(base64 || ""));
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  const getIdToken = async () => {
+    // Get ID Token from server
+    const res = await fetch("http://localhost:8080/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    return data?.token;
+  };
+
+  const login = async () => {
+    // trying logging in with the Single Factor Auth SDK
+    try {
+      if (!web3authSFAuth) {
+        uiConsole("Web3Auth Single Factor Auth SDK not initialized yet");
+        return;
+      }
+      setIsLoggingIn(true);
+      const idToken = await getIdToken();
+      console.log(idToken);
+      setIdToken(idToken!);
+      const { sub } = parseToken(idToken);
+      const web3authSfaprovider = await web3authSFAuth.connect({
+        verifier,
+        verifierId: sub,
+        idToken: idToken!,
+      });
+      if (web3authSfaprovider) {
+        setProvider(web3authSfaprovider);
+      }
+      setUsesSfaSDK(true);
+      setIsLoggingIn(false);
+    } catch (err) {
+      // Single Factor Auth SDK throws an error if the user has already enabled MFA
+      // One can use the Web3AuthNoModal SDK to handle this case
+      setIsLoggingIn(false);
+      console.error(err);
+    }
+  };
+
+  const getUserInfo = async () => {
+    if (usesSfaSDK) {
+      uiConsole(
+        "You are directly using Single Factor Auth SDK to login the user, hence the Web3Auth <code>getUserInfo</code> function won't work for you. Get the user details directly from id token.",
+        parseToken(idToken)
+      );
+      return;
+    }
+  };
+
+  const logout = async () => {
+    if (usesSfaSDK) {
+      console.log(
+        "You are directly using Single Factor Auth SDK to login the user, hence the Web3Auth logout function won't work for you. You can logout the user directly from your login provider, or just clear the provider object."
+      );
+      setProvider(null);
+      return;
+    }
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("No provider found");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const userAccount = await rpc.getAccounts();
+    uiConsole(userAccount);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("No provider found");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("No provider found");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const result = await rpc.signMessage();
+    uiConsole(result);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("No provider found");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const result = await rpc.signAndSendTransaction();
+    uiConsole(result);
+  };
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loginView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Get Accounts
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Get Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
+      </div>
+    </>
+  );
+
+  const logoutView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  );
+
+  return (
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+          Web3Auth
+        </a>{" "}
+        SFA Next JWT Example
+      </h1>
+
+      {isLoggingIn ? (
+        <Loading />
+      ) : (
+        <div className="grid">
+          {web3authSFAuth ? (provider ? loginView : logoutView) : null}
+        </div>
+      )}
+
+      <footer className="footer">
+        <a
+          href="https://github.com/Web3Auth/web3auth-core-kit-examples/tree/main/single-factor-auth-web/sfa-next-jwt-example "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
