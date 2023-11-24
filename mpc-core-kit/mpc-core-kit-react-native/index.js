abb8e995fb7643f19c950f5cc62c7005a846b39f
ieow <cwlee0@gmail.com>
2023-11-24 15:51:58 +0800

    feat: init rn for mpc

---
+++ mpc-core-kit/mpc-core-kit-react-native/index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import './globals';
+import 'react-native-get-random-values';
+import App from './App';
+import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => App);
