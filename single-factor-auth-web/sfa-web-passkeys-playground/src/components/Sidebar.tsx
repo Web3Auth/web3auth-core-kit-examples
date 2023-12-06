2005bfc81aaa8a2132f9dfe95877f62a12189347
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2023-12-06 21:37:39 +0530

    Update Sidebar.tsx

--- single-factor-auth-web/sfa-web-passkeys-playground/src/components/Sidebar.tsx
+++ single-factor-auth-web/sfa-web-passkeys-playground/src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useWeb3Auth } from "../services/web3auth";
 
 const Sidebar = () => {
-  const { provider, user, connectedChain } = useWeb3Auth();
+  const { connectedChain } = useWeb3Auth();
 
   const navigate = useNavigate();
   function goToHome() {
