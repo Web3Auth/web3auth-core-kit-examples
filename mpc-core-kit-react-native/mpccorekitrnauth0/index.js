472306b5adbd1bb90e04e85e306e6ee76e52ea24
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2024-04-15 04:03:53 +0530

    Add auth0 example

---
+++ mpc-core-kit-react-native/mpccorekitrnauth0/index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import './globals';
+import 'react-native-get-random-values';
+import {name as appName} from './app.json';
+
+import App from './App';
+
+AppRegistry.registerComponent(appName, () => App);
