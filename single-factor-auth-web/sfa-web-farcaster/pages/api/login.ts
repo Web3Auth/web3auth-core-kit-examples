4015ae11cac21a460d9484a3d0fc20617f738b4d
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-05-08 17:24:55 +0800

    Add Farcaster Examples
    with SFA Web

---
+++ single-factor-auth-web/sfa-web-farcaster/pages/api/login.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import jwt from "jsonwebtoken";
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ token: string }>
+) {
+  const userData = req.body.userData;
+  const privateKey = process.env.PRIVATE_KEY!;
+
+  const jwtToken = jwt.sign(
+    {
+      sub: userData?.fid.toString(),
+      name: userData?.displayName,
+      username: userData?.username,
+      profileImage: userData?.pfpUrl,
+      custody: userData?.custody,
+      aud: "w3a:farcaster-server",
+      iss: "https://web3auth.io",
+      iat: Math.floor(Date.now() / 1000),
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    },
+    privateKey,
+    { algorithm: "RS256", keyid: "49563d7e86f6626426be" }
+  );
+  res.status(200).json({ token: jwtToken });
+}
