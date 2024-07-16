1304ff6f3596f6cdfc76074a901a4141181f9328
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-07-16 11:50:34 +0530

    update sfa-flutter-quick-start

--- single-factor-auth-flutter/sfa_flutter_quick_start/lib/core/web3auth_sfa.dart
+++ single-factor-auth-flutter/sfa_flutter_quick_start/lib/core/web3auth_sfa.dart
@@ -5,6 +5,7 @@ import 'package:single_factor_auth_flutter/input.dart';
 import 'package:single_factor_auth_flutter/output.dart';
 // IMP START - Quick Start
 import 'package:single_factor_auth_flutter/single_factor_auth_flutter.dart';
+
 // IMP END - Quick Start
 class Web3AuthSFA {
   final SingleFactAuthFlutter singleFactAuthFlutter;
@@ -14,7 +15,11 @@ class Web3AuthSFA {
   Future<void> init() async {
     // IMP START - Initialize Web3Auth SFA
     await singleFactAuthFlutter.init(
-      Web3AuthNetwork(network: TorusNetwork.cyan),
+      SFAParams(
+        network: Web3AuthNetwork.mainnet,
+        clientid:
+            "BJRZ6qdDTbj6Vd5YXvV994TYCqY42-PxldCetmvGTUdoq6pkCqdpuC1DIehz76zuYdaq1RJkXGHuDraHRhCQHvA",
+      ),
     );
     // IMP END - Initialize Web3Auth SFA
   }
@@ -37,9 +42,9 @@ class Web3AuthSFA {
       final TorusKey torusKey = await singleFactAuthFlutter.getKey(
         LoginParams(
           // IMP START - Verifier Creation
-          verifier: 'web3auth-firebase-examples',
+          verifier: 'w3a-firebase-demo',
           // IMP END - Verifier Creation
-          verifierId: 'sfa.flutter@w3a.link',
+          verifierId: user.uid,
           idToken: token!,
         ),
       );
