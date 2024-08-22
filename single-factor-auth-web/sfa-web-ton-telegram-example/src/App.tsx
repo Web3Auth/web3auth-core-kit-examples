6421f31728c33ff5dac636d6e240595375ea803b
Maharshi Mishra <maharshi@tor.us>
2024-08-22 18:56:14 +0530

    Update App.tsx

--- single-factor-auth-web/sfa-web-ton-telegram-example/src/App.tsx
+++ single-factor-auth-web/sfa-web-ton-telegram-example/src/App.tsx
@@ -74,13 +74,20 @@ function App() {
         await web3authSfa.init();
       }
       const { payload } = decodeToken(idToken);
-      await web3authSfa.connect({
-        verifier,
-        verifierId: (payload as any).sub,
-        idToken: idToken!,
-      });
-      setIsLoggingIn(false);
-      setLoggedIn(true);
+      if(web3authSfa.status === 'ready') {
+        await web3authSfa.connect({
+          verifier,
+          verifierId: (payload as any).sub,
+          idToken: idToken!,
+        });
+        setIsLoggingIn(false);
+        setLoggedIn(true);
+      }
+      else if(web3authSfa.status === 'connected') {
+        await web3authSfa.authenticateUser();
+        setIsLoggingIn(false);
+        setLoggedIn(true);
+      }
     } catch (err) {
       // Single Factor Auth SDK throws an error if the user has already enabled MFA
       // One can use the Web3AuthNoModal SDK to handle this case
