d6414862f3376549915d8c144c30b7e504a024be
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-09-16 18:02:44 +0530

    Update Node App for QS

--- mpc-core-kit-web/quick-starts/mpc-core-kit-node-quick-start/index.js
+++ mpc-core-kit-web/quick-starts/mpc-core-kit-node-quick-start/index.js
@@ -1,21 +1,3 @@
-// Define required constants
-// IMP START - Dashboard Registration
-const web3AuthClientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
-// IMP END - Dashboard Registration
-// IMP START - Verifier Creation
-const firebaseVerifier = "w3a-firebase-demo";
-// IMP END - Verifier Creation
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB0nd9YsPLu-tpdCrsXn8wgsWVAiYEpQ_E",
-  authDomain: "web3auth-oauth-logins.firebaseapp.com",
-  projectId: "web3auth-oauth-logins",
-  storageBucket: "web3auth-oauth-logins.appspot.com",
-  messagingSenderId: "461819774167",
-  appId: "1:461819774167:web:e74addfb6cc88f3b5b9c92",
-};
-
-// Import required modules
 // IMP START - Quick Start
 const { EthereumSigningProvider } = require("@web3auth/ethereum-mpc-provider");
 const {
@@ -29,19 +11,29 @@ const {
   keyToMnemonic,
 } = require("@web3auth/mpc-core-kit");
 const { CHAIN_NAMESPACES } = require("@web3auth/base");
+const { CommonPrivateKeyProvider } = require("@web3auth/base-provider");
+const { Web3Auth } = require("@web3auth/node-sdk");
+const { Point, secp256k1 } = require("@tkey/common-types");
 // IMP END - Quick Start
 
 const { sign } = require("jsonwebtoken");
 const { BN } = require("bn.js");
 const { tssLib } = require("@toruslabs/tss-dkls-lib");
-const { CommonPrivateKeyProvider } = require("@web3auth/base-provider");
-const Web3AuthSingleFactorAuth = require("@web3auth/single-factor-auth").default;
 const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 const { initializeApp } = require("firebase/app");
 const fs = require("fs").promises;
 const path = require("path");
 const Web3 = require("web3").default;
 
+// IMP START - Dashboard Registration
+const web3AuthClientId = "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ";
+// IMP END - Dashboard Registration
+
+// IMP START - Verifier Creation
+const verifier = "w3a-node-demo";
+// IMP END - Verifier Creation
+
+// IMP START - Define Local Storage for MPC Core Kit
 // Define the file path for storage
 const storageFilePath = path.resolve(__dirname, "storage.json");
 
@@ -79,9 +71,7 @@ async function removeItem(key) {
   await writeStorageFile(storage);
 }
 
-// Declare evmProvider and coreKitInstance
-let evmProvider;
-let coreKitInstance;
+// IMP END - Define Local Storage for MPC Core Kit
 
 // IMP START - SDK Initialization
 // Chain configuration for Ethereum Mainnet
@@ -95,65 +85,30 @@ const chainConfig = {
   tickerName: "Ethereum",
 };
 
+const coreKitInstance = new Web3AuthMPCCoreKit({
+  web3AuthClientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.MAINNET,
+  storage: {
+    getItem: async (key) => getItem(key),
+    setItem: async (key, value) => setItem(key, value),
+    removeItem: async (key) => removeItem(key),
+  },
+  manualSync: true,
+  tssLib,
+  baseUrl: "http://localhost",
+  uxMode: "nodejs",
+});
+
+const evmProvider = new EthereumSigningProvider({ config: { chainConfig } });
+
+evmProvider.setupProvider(makeEthereumSigner(coreKitInstance));
+
 // Initialize CoreKit
 const initializeCoreKit = async () => {
-  coreKitInstance = new Web3AuthMPCCoreKit({
-    web3AuthClientId,
-    web3AuthNetwork: WEB3AUTH_NETWORK.MAINNET,
-    storage: {
-      getItem: async (key) => getItem(key),
-      setItem: async (key, value) => setItem(key, value),
-      removeItem: async (key) => removeItem(key),
-    },
-    manualSync: true,
-    tssLib,
-    baseUrl: "http://localhost",
-    uxMode: "nodejs",
-  });
-
-  evmProvider = new EthereumSigningProvider({ config: { chainConfig } });
-  evmProvider.setupProvider(makeEthereumSigner(coreKitInstance));
-
   await coreKitInstance.init();
   console.log("Core Kit Initialized, Status:", coreKitInstance.status);
 };
 // IMP END - SDK Initialization
-
-// IMP START - Auth Provider Login
-const getSocialMFAFactorKey = async () => {
-  try {
-    const privateKeyProvider = new CommonPrivateKeyProvider({ config: { chainConfig } });
-    const web3authSfa = new Web3AuthSingleFactorAuth({
-      clientId: web3AuthClientId,
-      web3AuthNetwork: WEB3AUTH_NETWORK.MAINNET,
-      usePnPKey: false,
-      privateKeyProvider,
-    });
-    await web3authSfa.init();
-
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const res = await signInWithEmailAndPassword(auth, "custom+jwt@firebase.login", "Testing@123");
-    const idToken = await res.user.getIdToken(true);
-    const userInfo = parseToken(idToken);
-
-    const web3authProvider = await web3authSfa.connect({
-      verifier: firebaseVerifier,
-      verifierId: userInfo.sub,
-      idToken,
-    });
-
-    const factorKey = await web3authProvider.request({
-      method: "private_key",
-    });
-
-    return factorKey;
-  } catch (err) {
-    console.error(err);
-    return "";
-  }
-};
-// IMP END - Auth Provider Login
 
 // Get Key Details
 const keyDetails = async () => {
@@ -169,6 +124,7 @@ const exportMnemonicFactor = async () => {
   await coreKitInstance.createFactor({
     shareType: TssShareType.RECOVERY,
     factorKey: recoveryFactorKey.private,
+    shareDescription: "Mnemonic Factor",
   });
   const factorKeyMnemonic = keyToMnemonic(recoveryFactorKey.private.toString("hex"));
   console.log("Mnemonic:", factorKeyMnemonic);
@@ -234,21 +190,82 @@ const signMessage = async (message) => {
 };
 // IMP END - Blockchain Calls
 
+// IMP START - Export Social Account Factor
+const getSocialMFAFactorKey = async () => {
+  try {
+    const web3auth = new Web3Auth({
+      clientId: web3AuthClientId,
+      web3AuthNetwork: "sapphire_mainnet", // Get your Network ID from Web3Auth Dashboard
+    });
+    const privateKeyProvider = new CommonPrivateKeyProvider({ config: { chainConfig } });
+
+    web3auth.init({ provider: privateKeyProvider });
+
+    const app = initializeApp({
+      apiKey: "AIzaSyB0nd9YsPLu-tpdCrsXn8wgsWVAiYEpQ_E",
+      authDomain: "web3auth-oauth-logins.firebaseapp.com",
+      projectId: "web3auth-oauth-logins",
+      storageBucket: "web3auth-oauth-logins.appspot.com",
+      messagingSenderId: "461819774167",
+      appId: "1:461819774167:web:e74addfb6cc88f3b5b9c92",
+    });
+
+    const auth = getAuth(app);
+    const res = await signInWithEmailAndPassword(auth, "custom+jwt@firebase.login", "Testing@123");
+    const idToken = await res.user.getIdToken(true);
+    const userInfo = parseToken(idToken);
+
+    const web3authProvider = await web3auth.connect({
+      verifier: "w3a-firebase-demo",
+      verifierId: userInfo.sub,
+      idToken,
+    });
+
+    const factorKey = await web3authProvider.request({
+      method: "private_key",
+    });
+
+    return factorKey;
+  } catch (err) {
+    console.error(err);
+    return "";
+  }
+};
+// IMP END - Export Social Account Factor
+
+// IMP START - Delete Factor
+const deleteFactor = async () => {
+  let factorPub;
+  for (const [key, value] of Object.entries(coreKitInstance.getKeyDetails().shareDescriptions)) {
+    if (value.length > 0) {
+      const parsedData = JSON.parse(value[0]);
+      if (parsedData.module === "Social Factor") {
+        factorPub = key;
+      }
+    }
+  }
+  console.log("Factor Pub:", factorPub);
+  const pub = Point.fromSEC1(secp256k1, factorPub);
+  await coreKitInstance.deleteFactor(pub);
+  await coreKitInstance.commitChanges();
+};
+// IMP END - Delete Factor
+
 // Perform JWT Login and MFA Setup
 const initAndLogin = async () => {
   await initializeCoreKit();
 
   if (coreKitInstance.status === COREKIT_STATUS.LOGGED_IN) {
-    console.log("Already logged in. Logging out and retrying...");
+    console.log("\x1b[33m%s\x1b[0m", "Already logged in. Logging out and retrying...");
     await coreKitInstance.logout();
     await coreKitInstance.init();
-    console.log("Core Kit reinitialized after logout, Status:", coreKitInstance.status);
+    console.log("\x1b[33m%s\x1b[0m", "Core Kit reinitialized after logout, Status:", coreKitInstance.status);
   }
 
   if (coreKitInstance.status !== COREKIT_STATUS.INITIALIZED) {
     throw new Error("Failed to initialize Web3AuthMPCCoreKit");
   }
-
+  // IMP START - Auth Provider Login
   const privateKey = await fs.readFile("privateKey.pem", "utf8");
   const sub = Math.random().toString(36).substring(7);
 
@@ -266,36 +283,67 @@ const initAndLogin = async () => {
     { algorithm: "RS256", keyid: "2ma4enu1kdvw5bo9xsfpi3gcjzrt6q78yl0h" }
   );
 
-  console.log("JWT Token:", token);
+  console.log("\x1b[33m%s\x1b[0m", "JWT Token:", token);
+  // IMP END - Auth Provider Login
 
+  // IMP START - Login
   await coreKitInstance.loginWithJWT({
-    verifier: "w3a-node-demo",
+    verifier,
     verifierId: sub,
     idToken: token,
   });
+  // IMP END - Login
 
-  console.log("Login initiated, Status:", coreKitInstance.status);
+  console.log("\x1b[33m%s\x1b[0m", "Login initiated, Status:", coreKitInstance.status);
 
   if (coreKitInstance.status === COREKIT_STATUS.LOGGED_IN) {
     await coreKitInstance.commitChanges();
-    console.log("Login successful, Status:", coreKitInstance.status);
+
+    console.log("\x1b[33m%s\x1b[0m", "Login successful, Status:", coreKitInstance.status);
 
     // Log the results of various methods after login
     await getUserInfo(); // Get User Info
+    await keyDetails(); // Get Key Details
+
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      "Account Created, it contains a hashed share for one click login for user. Enabling MFA in the next step using a social mfa factor."
+    );
+
     const socialFactorKey = new BN(await getSocialMFAFactorKey(), "hex");
-    console.log("Social Factor Key:", socialFactorKey.toString("hex"));
-    await coreKitInstance.enableMFA({ socialFactorKey });
+
+    console.log("\x1b[33m%s\x1b[0m", "Social Factor Key:", socialFactorKey.toString("hex"));
+
+    await coreKitInstance.enableMFA({ socialFactorKey, shareDescription: "Social Factor" });
     await coreKitInstance.commitChanges();
-    console.log("MFA Enabled");
+    await keyDetails(); // Get Key Details
+
+    console.log("\x1b[33m%s\x1b[0m", "MFA Enabled. Hashed key is deleted, device and social factor are enabled.");
 
     await getDeviceFactor(); // Get Device Factor
+
+    console.log("\x1b[33m%s\x1b[0m", "Exporting a new Mnemonic Factor for recovery.");
+
     await exportMnemonicFactor(); // Export Mnemonic for recovery
     await keyDetails(); // Get Key Details
+
+    console.log("\x1b[33m%s\x1b[0m", "Doing some blockchain interactions...");
 
     // Blockchain interaction
     await getAccounts(); // Get Ethereum Accounts
     await getBalance(); // Get Ethereum Balance
     await signMessage("Hello Web3Auth!"); // Sign a Message
+
+    console.log("\x1b[33m%s\x1b[0m", "Blockchain interactions done.");
+    console.log("\x1b[33m%s\x1b[0m", "Deleting Social MFA Factor and getting key details.");
+
+    await deleteFactor(); // Delete Social MFA Factor
+    await keyDetails(); // Get Key Details
+
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      "Social MFA Factor deleted. Note the pubs details related to hashed and social factor are empty in the share descriptions."
+    );
   }
 };
 
