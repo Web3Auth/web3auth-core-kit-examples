711204908e3f87c7f45f3aad02ebc860fe1e5148
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2024-09-18 13:31:56 +0530

    Add mpc ck expo example

---
+++ mpc-core-kit-react-native/mpc-core-kit-rn-expo-auth0/metro.config.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("metro-config");
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  const defaultSourceExts = [...sourceExts, "svg", "mjs", "cjs"];
+
+  return {
+    resolver: {
+      // IMP START - Bundler Issues
+      extraNodeModules: {
+        assert: require.resolve("empty-module"), // assert can be polyfilled here if needed
+        http: require.resolve("empty-module"), // stream-http can be polyfilled here if needed
+        https: require.resolve("empty-module"), // https-browserify can be polyfilled here if needed
+        os: require.resolve("empty-module"), // os-browserify can be polyfilled here if needed
+        url: require.resolve("empty-module"), // url can be polyfilled here if needed
+        zlib: require.resolve("empty-module"), // browserify-zlib can be polyfilled here if needed
+        path: require.resolve("empty-module"),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        buffer: require.resolve("buffer"),
+      },
+      // IMP END - Bundler Issues
+
+      // assetExts: assetExts.filter(ext => ext !== 'svg'),
+      // @ts-ignore
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+
+      sourceExts: process.env.TEST_REACT_NATIVE ? ["e2e.js"].concat(defaultSourceExts) : defaultSourceExts,
+      //   ? ['e2e.js'].concat(defaultSourceExts)
+      //   : defaultSourceExts,
+    },
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: true,
+          inlineRequires: true,
+        },
+      }),
+      // This detects entry points of React app and transforms them
+      // For the other files this will switch to use default `metro-react-native-babel-transformer` for transforming
+      babelTransformerPath: require.resolve("react-native-react-bridge/lib/plugin"),
+    },
+  };
+})();
