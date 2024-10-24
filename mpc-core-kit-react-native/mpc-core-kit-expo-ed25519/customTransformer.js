3c86e3252d27b0ec4abafb253801ee0d493603d3
ieow <cwlee0@gmail.com>
2024-10-24 13:41:28 +0800

    fix: cleanup

--- mpc-core-kit-react-native/mpc-core-kit-expo-ed25519/customTransformer.js
+++ mpc-core-kit-react-native/mpc-core-kit-expo-ed25519/customTransformer.js
@@ -1,22 +1,19 @@
-
 const { nodeModulesPolyfillPlugin } = require("esbuild-plugins-node-modules-polyfill");
 const reactNativeReactBridgeTransformer = require("react-native-react-bridge/lib/plugin");
 const esbuildOptions = {
-    plugins: [
-        nodeModulesPolyfillPlugin(
-            {
-                globals : {
-                    Buffer : true,
-                    crypto : true,
-                }, 
-                // modules: {
-                //     Buffer : true,
-                // }
-            } 
-        ),
-    ],
+  plugins: [
+    nodeModulesPolyfillPlugin({
+      globals: {
+        Buffer: true,
+        crypto: true,
+      },
+      // modules: {
+      //     Buffer : true,
+      // }
+    }),
+  ],
 };
 module.exports.transform = function ({ src, filename, options }) {
-    const transform = reactNativeReactBridgeTransformer.createTransformer(esbuildOptions);
-    return transform({ src, filename, options });
+  const transform = reactNativeReactBridgeTransformer.createTransformer(esbuildOptions);
+  return transform({ src, filename, options });
 };
