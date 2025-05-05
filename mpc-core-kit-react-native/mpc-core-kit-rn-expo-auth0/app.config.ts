c1d0861e51f57db05c8bf259d5f3fe15afbcbbd6
ieow <cwlee0@gmail.com>
2025-05-05 11:03:21 +0800

    feat: new Architecture

--- mpc-core-kit-react-native/mpc-core-kit-rn-expo-auth0/app.config.ts
+++ mpc-core-kit-react-native/mpc-core-kit-rn-expo-auth0/app.config.ts
@@ -6,6 +6,13 @@ const config: ExpoConfig = {
   name: "my-app",
   slug: "my-app",
   plugins: ["expo-secure-store"],
+  newArchEnabled: true,
+  ios: {
+    newArchEnabled: true,
+  },
+  android: {
+    newArchEnabled: true,
+  },
 };
 
 export default config;
